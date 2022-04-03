07756873afcdd2cb1fc99e57d2bce6890dd063f8
LiemNH <liem.nguyehoang.127@gmail.com>
2022-04-03 22:59:22 +0700

    [LiemNH] Enhancing the animation with custom transitions

---
+++ Honeymoon/Extension/CardTransition.swift
@@ -0,0 +1,19 @@
+//
+//  CardTransition.swift
+//  Honeymoon
+//
+//  Created by LiemNH2 on 03/04/2022.
+//
+
+import SwiftUI
+
+extension AnyTransition {
+    static var trailingBottom: AnyTransition {
+        AnyTransition
+            .asymmetric(insertion: .identity, removal: .move(edge: .trailing).combined(with: .move(edge: .bottom)))
+    }
+    
+    static var leadingBottom: AnyTransition {
+        AnyTransition.asymmetric(insertion: .identity, removal: AnyTransition.move(edge: .leading).combined(with: .move(edge: .bottom)))
+    }
+}
