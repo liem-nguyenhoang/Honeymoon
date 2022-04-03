12b134f8b73629eddde062f847c12b1cb8c16694
LiemNH <liem.nguyehoang.127@gmail.com>
2022-04-03 22:31:46 +0700

    [LiemNH] Swiping motion

--- Honeymoon/View/ContentView.swift
+++ Honeymoon/View/ContentView.swift
@@ -13,6 +13,7 @@ struct ContentView: View {
     @State var showAlert: Bool = false
     @State var showGuild: Bool = false
     @State var showInfo: Bool = false
+    @GestureState private var dragState = DragState.inactive
     
     // MARK: - CARD VIEWS
     
@@ -33,6 +34,40 @@ struct ContentView: View {
         return index == 0
     }
     
+    // MARK: DRAG STATES
+    enum DragState {
+        case inactive
+        case pressing
+        case dragging(translation: CGSize)
+        
+        var translation: CGSize {
+            switch self {
+            case .inactive, .pressing:
+                return .zero
+            case .dragging(let translation):
+                return translation
+            }
+        }
+        
+        var isDragging: Bool {
+            switch self {
+            case .dragging:
+                return true
+            case .pressing, .inactive:
+                return false
+            }
+        }
+        
+        var isPressing: Bool {
+            switch self {
+            case .pressing, .dragging:
+                return true
+            case .inactive:
+                return false
+            }
+        }
+    }
+    
     // MARK: - BODY
     
     var body: some View {
@@ -40,6 +75,8 @@ struct ContentView: View {
             // MARK: - HEADER
             
             HeaderView(showGuideView: $showGuild,showInfoView: $showInfo)
+                .opacity(dragState.isDragging ? 0.0 : 1.0)
+                .animation(.default)
             
             Spacer()
             
@@ -49,6 +86,38 @@ struct ContentView: View {
                 ForEach(cardViews){cardView in
                     cardView
                         .zIndex(self.isTopCard(cardView: cardView) ? 1 : 0)
+                        .offset(
+                            x: self.isTopCard(cardView: cardView) ?  self.dragState.translation.width : 0,
+                            y: self.isTopCard(cardView: cardView) ? self.dragState.translation.height : 0
+                        )
+                        .scaleEffect(self.dragState.isDragging && self.isTopCard(cardView: cardView) ? 0.85 : 1.0)
+                        .rotationEffect(
+                            Angle(
+                                degrees: self.isTopCard(cardView: cardView) ? Double(self.dragState.translation.width / 12) : 0
+                            )
+                        )
+                        .animation(
+                            .interpolatingSpring(
+                                stiffness: 120,
+                                damping: 120
+                            )
+                        )
+                        .gesture(
+                            LongPressGesture(minimumDuration: 0.01)
+                                .sequenced(
+                                    before: DragGesture()
+                                )
+                                .updating(self.$dragState, body: {(value, state, transaction) in
+                                    switch value {
+                                    case .first(true):
+                                        state = .pressing
+                                    case .second(true, let drag):
+                                        state = .dragging(translation: drag?.translation ?? .zero)
+                                    default:
+                                        break
+                                    }
+                                })
+                        )
                 }
             }
             
@@ -56,6 +125,8 @@ struct ContentView: View {
             
             // MARK: - FOOTER
             FooterView(showBookingAlert: $showAlert)
+                .opacity(dragState.isDragging ? 0.0 : 1.0)
+                .animation(.default)
         }
         .alert(isPresented: $showAlert) {
             Alert(title: Text("SUCCESS"), message:
