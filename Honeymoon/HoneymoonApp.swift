482b2b01e497d4b44ea87537e56c163e041599c5
LiemNH <liem.nguyehoang.127@gmail.com>
2022-04-03 17:51:58 +0700

    [LiemNH] Set up project

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by LiemNH2 on 03/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
